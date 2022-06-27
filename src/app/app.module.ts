0d4a1229998cfd2882c2175075dc46caa108d37a
Kevin Cai <kevin_cai@epam.com>
2022-06-27 13:53:14 +0800

    feat: task item completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ButtonComponent } from './component/button/button.component';
+import { TasksComponent } from './component/tasks/tasks.component';
+import { TaskItemComponent } from './component/task-item/task-item.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, ButtonComponent, TasksComponent, TaskItemComponent],
+  imports: [BrowserModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/component/tasks/tasks.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { Tasks } from 'src/app/mock-task';
+import { Task } from 'src/app/TASK';
+
+@Component({
+  selector: 'app-tasks',
+  templateUrl: './tasks.component.html',
+  styleUrls: ['./tasks.component.css'],
+})
+export class TasksComponent implements OnInit {
+  tasks: Task[] = Tasks;
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}

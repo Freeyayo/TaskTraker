4e4e3265190879c60c2924eb911ccc3e5e25273a
Kevin Cai <kevin_cai@epam.com>
2022-06-28 16:11:20 +0800

    feat: enable http functionality

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { ButtonComponent } from './component/button/button.component';
@@ -7,8 +8,13 @@ import { TasksComponent } from './component/tasks/tasks.component';
 import { TaskItemComponent } from './component/task-item/task-item.component';
 
 @NgModule({
-  declarations: [AppComponent, ButtonComponent, TasksComponent, TaskItemComponent],
-  imports: [BrowserModule],
+  declarations: [
+    AppComponent,
+    ButtonComponent,
+    TasksComponent,
+    TaskItemComponent,
+  ],
+  imports: [BrowserModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/service/task.service.ts
+++ src/app/service/task.service.ts
@@ -2,15 +2,22 @@ import { Injectable } from '@angular/core';
 import { Tasks } from '../mock-task';
 import { Task } from '../TASK';
 import { Observable, of } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root',
 })
 export class TaskService {
-  constructor() {}
+  private apiUrl = 'http://localhost:5000/tasks';
+
+  constructor(private http: HttpClient) {}
+
+  // getTasks(): Observable<Task[]> {
+  //   const tasks = of(Tasks);
+  //   return tasks;
+  // }
 
   getTasks(): Observable<Task[]> {
-    const tasks = of(Tasks);
-    return tasks;
+    return this.http.get<Task[]>(this.apiUrl);
   }
 }
